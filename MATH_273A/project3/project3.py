97047b7b403de1fcab563d52fbcd06275e6b9561
Eric Weise <ericdweise@gmail.com>
2019-11-30 19:06:19 -0800

    Preconditioning and gradient descent added to project_3.py

--- MATH_273A/project3/project3.py
+++ MATH_273A/project3/project3.py
@@ -4,9 +4,12 @@ from math import exp
 from math import pi
 from math import sin
 from math import sqrt
+from matrix_toolset import iter_matrix
+from cholesky_factorization import incomplete_cholesky_factorization
+from congugate_gradient import congugate_gradient
 
 
-def create_grids():
+def create_mesh():
     xmin = -1
     ymin = -1
     xmax = 1
@@ -18,14 +21,27 @@ def create_grids():
     return xgrid, ygrid
 
 
-def define_ellipse_func(xcent, a, ycent, b):
+def define_regions(xcent, a, ycent, b):
     """Create a function that defines the interior of an ellipse"""
+
     def func(x,y):
-        """returns True if point (x,y) is inside of the ellipse. False otherwise."""
-        if ((x-xcent)/a)**2 + ((y-ycent)/b)**2 < 1:
-            return True
-        else:
-            return False
+        """Given a point returns what region the point is in:
+        OMEGA: The point is in the ellipse interior
+        dOMEGA: The point is on the edge of the ellipse
+        D: The point is outside of the ellipse but not on dD
+        dD: The point is on the boundary of the computational domain
+        """
+        if (x==-1) or (x==1) or (y==-1) or (y==-1):
+            return 'dD'
+
+        v = ((x-xcent)/a)**2 + ((y-ycent)/b)**2
+        if v < 1:
+            return 'OMEGA'
+        if v == 1:
+            return 'dOMEGA'
+
+        return 'D'
+
     return(func)
 
 
@@ -39,45 +55,14 @@ def func_g(x,y):
     return(exp(x)*cos(2*pi*y))
 
 
-def is_sym_pos_def(A):
-    """Test if a matrix A is symmetric positive definite"""
-    for i in range(A.shape[0]):
-        if not np.linalg.det(A[:i+1,:i+1])>0:
-            return False
-
-        for j in range(A.shape[1]):
-            if i != j:
-                if A[i,j] != A[j,i]:
-                    return False
-
-    return True
-
-
-def incomplete_cholesky_factorization(A):
-    """Compute the sparse Upper Right Triangular matrix R in R^T*R=A"""
-    R = np.zeros(A.shape)
-    for i in range(A.shape[0]):
-        r = A[i,i]
-        for k in range(i-1):
-            r -= R[k,i]**2
-        R[i,i] = sqrt(r)
-
-        for j in range(i, A.shape[1]):
-            # "Incomplete" factorization
-            if A[i,j] == 0:
-                continue
-
-            r = A[i,j]
-            for k in range(i-1):
-                r -= R[k,i]*R[k,j]
-            R[i,j] = r/R[i,i]
-
-    return R
-
-
-def build_soe_matrix(ellipse_func, xgrid, ygrid):
+def build_soe_matrix(find_region, xgrid, ygrid):
     """Will create a matrix that represents the system of equations"""
-    pass
+
+    ashape = xgrid.shape[0]*xgrid.shape[1]
+    A = np.zeros((ashape, ashape))
+
+    for row, col in iter_matrix(xgrid):
+        region = find_region(row, col)
 
 
 def add_ghost_fluid_point():
@@ -90,12 +75,27 @@ def compute_residual():
 
 if __name__ == '__main__':
 
+    # Cutoff point
+    residual = 10**(-7)
+
     # DEFINE ELLIPSE
     XCENTER = 0.5
     YCENTER = 0.5
     A = 0.1
     B = 0.25
-    is_in_ellipse = define_ellipse_func(XCENTER, A, YCENTER, B)
+    find_region = define_regions(XCENTER, A, YCENTER, B)
 
     # CREATE MATRIX FOR SYSTEM OF EQUATIONS
-    soe_matrix = build_soe_matrix(is_in_ellipse, func_f, func_g)
+    A = build_soe_matrix(find_region, func_f, func_g)
+
+    # Find Incomplete Cholesky Factorization
+    R = incomplete_cholesky_factorization(A)
+    R_inverse = np.linalg.inv(R)
+    R_transpose_inverse = np.linalg.inv(np.matrix.transpose(R))
+
+    # Precondition Matrix:
+    A_hat = np.matmul(np.matmul(R_transpose_inverse, A), R_inverse)
+    b_hat = np.matmul(R_transpose_inverse, b)
+    x_hat = congugate_gradient()
+
+    x = np.matmul(R_inverse, x_hat)
