961900e88a360a9cd2b9e4f421553aecba95d388
Eric Weise <ericdweise@gmail.com>
2019-11-30 19:05:49 -0800

    Added incomplete_cholesky_factorization to cholesky_factorization.py

---
+++ MATH_273A/project3/cholesky_factorization.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+def incomplete_cholesky_factorization(A):
+    """Compute the sparse Upper Right Triangular matrix R in R^T*R=A"""
+    R = np.zeros(A.shape)
+    for i in range(A.shape[0]):
+        r = A[i,i]
+        for k in range(i-1):
+            r -= R[k,i]**2
+        R[i,i] = sqrt(r)
+
+        for j in range(i, A.shape[1]):
+            # "Incomplete" factorization
+            if A[i,j] == 0:
+                continue
+
+            r = A[i,j]
+            for k in range(i-1):
+                r -= R[k,i]*R[k,j]
+            R[i,j] = r/R[i,i]
+
+    return R
